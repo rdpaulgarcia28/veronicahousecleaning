4d6453dcc37b517706d38cf1f65de56b22cc7907
Paul Garcia <richardplgarcia@gmail.com>
2022-02-18 01:17:30 -0700

    nueva version

---
+++ js/hamburger.js
@@ -0,0 +1,22 @@
+const hamburgerBottton = document.querySelector("#btn-hamburger"); //DIV
+const hamburgerPanel = document.querySelector("#panel"); //DIV
+const btnPanel1 = document.querySelector("#btn-panel-1");
+const btnPanel2 = document.querySelector("#btn-panel-2");
+const btnPanel3 = document.querySelector("#btn-panel-3");
+const btnPanel4 = document.querySelector("#btn-panel-4");
+
+hamburgerBottton.onclick = function menuHamburguer() {
+    hamburgerPanel.classList.toggle("is-active");    //AL ELEMENTO hamburguerPanel SE LE AGREGARA O QUITARA LA CLASE 
+}
+btnPanel1.onclick = function menuHamburguer() {
+    hamburgerPanel.classList.toggle("is-active");    //AL ELEMENTO hamburguerPanel SE LE AGREGARA O QUITARA LA CLASE 
+}
+btnPanel2.onclick = function menuHamburguer() {
+    hamburgerPanel.classList.toggle("is-active");    //AL ELEMENTO hamburguerPanel SE LE AGREGARA O QUITARA LA CLASE 
+}
+btnPanel3.onclick = function menuHamburguer() {
+    hamburgerPanel.classList.toggle("is-active");    //AL ELEMENTO hamburguerPanel SE LE AGREGARA O QUITARA LA CLASE 
+}
+btnPanel4.onclick = function menuHamburguer() {
+    hamburgerPanel.classList.toggle("is-active");    //AL ELEMENTO hamburguerPanel SE LE AGREGARA O QUITARA LA CLASE 
+}
